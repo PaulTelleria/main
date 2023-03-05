b04aa111289087e1dd9707f8a122575e2faa3642
Paul Telleria <69241157+PaulTelleria@users.noreply.github.com>
2023-03-05 18:42:26 +0100

    Add files via upload

--- BigData_Processing/StreamingJob.scala
+++ BigData_Processing/StreamingJob.scala
@@ -1,45 +1,42 @@
-package project.streaming
+package practica.streaming
 
-import java.sql.Timestamp
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
-
 import org.apache.spark.sql.{DataFrame, SparkSession}
-
-case class UsersMessage(timestamp: Long, id: String, antenna_id: String, bytes: Long, app: String )
 
 trait StreamingJob {
 
+  def infoTotal(data: DataFrame, aggColumn: String): DataFrame
+  def writeToJdbc(dataFrame: DataFrame, jdbcURI: String, jdbcTable: String, user: String, password: String): Future[Unit]
+
+  def writeToStorage(dataFrame: DataFrame, storageRootPath: String): Future[Unit]
   val spark: SparkSession
 
   def readFromKafka(kafkaServer: String, topic: String): DataFrame
-
   def parserJsonData(dataFrame: DataFrame): DataFrame
 
-  def readUserMetadata(jobid: Text, jobname: Text, jobemail: Text, jobquota: Bigint): DataFrame
 
-  def enrichuserWithMetadata(userDF: DataFrame, metadataDF: DataFrame): DataFrame
-
-  def computeDevicesCountByCoordinates(dataFrame: DataFrame): DataFrame
-
-  def writeToJdbc(dataFrame: DataFrame, jobid: Text, jobname: Text, jobemail: Text, jobquota: Bigint): Future[Unit]
-
-  def writeToStorage(dataFrame: DataFrame, storageRootPath: String): Future[Unit]
 
   def run(args: Array[String]): Unit = {
-    val Array(kafkaServer, topic, jdbcUri, jdbcMetadataTable, aggJdbcTable, jdbcUser, jdbcPassword, storagePath) = args
+    val Array(kafkaServer, topic, jdbcUri, aggJdbcTable, jdbcUser, jdbcPassword, storagePath) = args
     println(s"Running with: ${args.toSeq}")
 
     val kafkaDF = readFromKafka(kafkaServer, topic)
-    val userDF = parserJsonData(kafkaDF)
-    val metadataDF = readAntennaMetadata(jobid, jobname, jobemail, jobquota)
-    val userMetadataDF = enrichuserWithMetadata(userDF, metadataDF)
-    val storageFuture = writeToStorage(userDF, storagePath)
-    val aggByCoordinatesDF = computeDevicesCountByCoordinates(antennaMetadataDF)
-    val aggFuture = writeToJdbc(aggByCoordinatesDF, jdbcUri, aggJdbcTable, jdbcUser, jdbcPassword)
+    val parserDF = parserJsonData(kafkaDF)
+    val storageFuture = writeToStorage(parserDF, storagePath)
 
-    Await.result(Future.sequence(Seq(aggFuture, storageFuture)), Duration.Inf)
+    val appStream = bytesTotalAgg(parsedDF, "app")
+    val userStream = bytesTotalAgg(parsedDF.withColumnRenamed("id", "user"), "user")
+    val antennaStream = bytesTotalAgg(parsedDF.withColumnRenamed("antenna_id", "antenna"), "antenna")
+
+    val appJdbc = writeToJdbc(appStream, jdbcUri, aggJdbcTable, jdbcUser, jdbcPassword)
+    val userJdbc = writeToJdbc(userStream, jdbcUri, aggJdbcTable, jdbcUser, jdbcPassword)
+    val antennaJdbc = writeToJdbc(antennaStream, jdbcUri, aggJdbcTable, jdbcUser, jdbcPassword)
+
+    Await.result(
+      Future.sequence(Seq(storageFuture, appJdbc, userJdbc, antennaJdbc)), Duration.Inf
+    )
 
     spark.close()
   }
